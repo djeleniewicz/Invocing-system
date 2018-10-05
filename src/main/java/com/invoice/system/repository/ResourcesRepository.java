43517e3f652be5ec2b2c24da688ee863a09fab47
djeleniewicz <dominik.jeleniewicz@gmail.com>
2018-10-05 13:28:29 +0200

    Edited repositories

--- src/main/java/com/invoice/system/repository/ResourcesRepository.java
+++ src/main/java/com/invoice/system/repository/ResourcesRepository.java
@@ -1,10 +1,11 @@
 package com.invoice.system.repository;
 
+import com.invoice.system.entity.Resources;
 import com.invoice.system.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ResourcesRepository extends JpaRepository<Seller, Integer> {
+public interface ResourcesRepository extends JpaRepository<Resources, Integer> {
 
 }
--- src/main/java/com/invoice/system/repository/RoleRepository.java
+++ src/main/java/com/invoice/system/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.invoice.system.repository;
 
+import com.invoice.system.entity.Role;
 import com.invoice.system.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Seller, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
